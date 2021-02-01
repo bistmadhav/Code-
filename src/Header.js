179ce487cb3ff44a74bd295c73946b0d8370dd67
Madhav Bist <bistmadhav5@gmail.com>
2021-01-31 19:24:58 -0600

    Added: Header Component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className = "app_header">
+             <h1> eSale </h1>
+        </div>
+    )
+}
+
+export default Header
